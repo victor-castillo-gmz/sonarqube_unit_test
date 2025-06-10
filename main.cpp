828f6a8835cf81f9f9a92c14303f602358b7ba09
victorcastgomz06 <a00841428@tec.mx>
2025-06-09 22:42:17 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "sum.h"
 #include "sub.h"
 #include "div.h"
+#include "mul.h" // Include our new multiplication header
+
 
 using std::cout;
 
@@ -11,5 +13,6 @@ int main() {
     std::cout << "sum(1, 5) = " << sum(1, 5) << '\n';
     std::cout << "sub(5, 1) = " << sub(5, 1) << '\n';
     std::cout << "div(9, 3) = " << divide(9, 3) << '\n';
+    std::cout << "mul(4, 2) = " << mul(4, 2) << '\n'; // Call  new mul method
     return 0;
 }
